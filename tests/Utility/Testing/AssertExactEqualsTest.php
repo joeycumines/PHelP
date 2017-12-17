3f61012fe935a7debc4315a9108138916e392115
Joey Cumines <joeycumines@gmail.com>
2017-12-17 23:36:43 +1000

    fix(tests): ci fix and test fix

--- tests/Utility/Testing/AssertExactEqualsTest.php
+++ tests/Utility/Testing/AssertExactEqualsTest.php
@@ -29,8 +29,8 @@ class AssertExactEqualsTest extends TestCase
 
         $calls = $case->getCalls();
         $this->assertCount(1, $calls, 'unexpected number of calls to assertTrue');
-        $callCondition = $calls[0];
-        $callMessage = $calls[1];
+        $callCondition = $calls[0][0];
+        $callMessage = $calls[0][1];
         $this->assertEquals($equal, $callCondition, "Unexpected evaluation of assertExactEquals: \n" . $callMessage);
         $this->assertTrue(is_bool($callCondition));
 
