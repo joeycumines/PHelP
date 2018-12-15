7f37ed32291346a3883abc367c3adea52495cebd
Joseph Cumines <joeycumines@gmail.com>
2018-12-15 15:04:48 +1000

    fix(ci): disable xdebug on ci

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-# We need to install dependencies only for Docker
 [[ ! -e /.dockerenv ]] && exit 0
 
 set -xe
 
-# Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
 apt-get install git -yqq
-
-# Install phpunit, the tool that we will use for testing
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit-5.7.phar -L
 chmod +x /usr/local/bin/phpunit
-
-# Install Xdebug
-pecl install xdebug
-# Enable Xdebug
-docker-php-ext-enable xdebug
-
 cp ci/php.ini /usr/local/etc/php/conf.d/test.ini
